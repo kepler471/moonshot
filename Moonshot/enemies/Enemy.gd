ea1324bf3178923e4fc6a16b9a4ea31d366128d7
Jamesbarford <jamesbarfordevans@gmail.com>
2020-11-09 17:31:28 +0000

    better scalling and change of direction

--- Moonshot/enemies/Enemy.gd
+++ Moonshot/enemies/Enemy.gd
@@ -1,25 +1,33 @@
 extends KinematicBody2D
 
-const GRAVITY:= 10;
-const SPEED:= 30;
-const FLOOR:= Vector2(0, -1);
+const GRAVITY:= 10
+const SPEED:= 75
+const FLOOR:= Vector2(0, -1)
 const Animations:= {
 	"GHOST": "ghost"
 }
 
-var velocity := Vector2();
-
 enum Directions {
-	LEFT = 0,
+	LEFT = -1,
 	RIGHT = 1
 }
 
-func _ready():
-	pass;
+var direction: int = Directions.RIGHT
+var velocity:= Vector2();
 
 func _process(delta) -> void:
-	velocity.x = SPEED;
-	$AnimatedSprite.play(Animations.GHOST);
-	velocity.y += GRAVITY;
-	velocity = move_and_slide(velocity, FLOOR);
+	velocity.x = SPEED * direction
+	$AnimatedSprite.play(Animations.GHOST)
+	velocity.y += GRAVITY
+	velocity = move_and_slide(velocity, FLOOR)
+
+	if is_on_wall():
+		$AnimatedSprite.flip_h = !$AnimatedSprite.flip_h
+		direction = change_direction(direction)
+
 	pass;
+
+func change_direction(direction: int) -> int:
+	if direction == Directions.LEFT:
+		return Directions.RIGHT
+	return Directions.LEFT
