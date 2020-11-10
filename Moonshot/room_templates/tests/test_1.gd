ce65689ee229fa2bd577917eec6a573855f92653
Stelios Georgiou <18307819+kepler471@users.noreply.github.com>
2020-11-10 18:11:35 +0000

    Update test_1.gd
    add debug note

--- Moonshot/room_templates/tests/test_1.gd
+++ Moonshot/room_templates/tests/test_1.gd
@@ -26,4 +26,4 @@ func _ready():
 		$Player/Camera2D.limit_bottom = $camera_limit_SE.position.y
 
 
-	print(get_tree().get_nodes_in_group("Baddies"))
+	print("DEBUG: List all Baddies in this room", get_tree().get_nodes_in_group("Baddies"))
