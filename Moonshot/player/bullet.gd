2742bba1a93b0c826b5d7dc9f6a26c30a22205aa
Stelios Georgiou <18307819+kepler471@users.noreply.github.com>
2020-11-10 18:10:48 +0000

    Update bullet.gd
    add debugging note

--- Moonshot/player/bullet.gd
+++ Moonshot/player/bullet.gd
@@ -7,7 +7,7 @@ func _ready():
 	apply_impulse(Vector2(100,100).rotated(rotation), Vector2(f_mag, 0).rotated(rotation))
 
 func _on_any_body_entered(body):
-	print(body)
+	print("DEBUG: Colliding with body ->", body)
 	get_node("CollisionShape2D").set_deferred("disabled", true)
 	if body.is_in_group("Baddies"):
 		body.on_hit(damage)
