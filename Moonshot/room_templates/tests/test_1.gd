a0fac74929a1e5b3846b2c1ea8d14daeb81cdec2
Stelios Georgiou <steliosdg@outlook.com>
2020-11-13 12:08:36 +0000

    (rebase) importing Player system on DEV

--- Moonshot/room_templates/tests/test_1.gd
+++ Moonshot/room_templates/tests/test_1.gd
@@ -1,13 +1,17 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+
+#export(PackedScene) var StartLevel := preload("res://room_templates/tests/test_1.tscn")
+
+#var visited_checkpoints := {}
+#var level: Node2D = null
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
+#	LevelLoader.setup(self, $player, StartLevel)
+# warning-ignore:return_value_discarded
+#	Events.connect("checkpoint_visited", self, "_on_Events_checkpoint_visited")
 
 	if get_node("camera_limit_NW") == null || get_node("camera_limit_SE") == null:
 		var map_limits = get_node("TileMap").get_used_rect()
@@ -26,4 +30,18 @@ func _ready():
 		$Player/Camera2D.limit_bottom = $camera_limit_SE.position.y
 
 
-	print("DEBUG: List all Baddies in this room", get_tree().get_nodes_in_group("Baddies"))
+#func _on_Events_checkpoint_visited(checkpoint_name: String) -> void:
+#	visited_checkpoints[level.name] = visited_checkpoints.get(level.name, [])
+#	visited_checkpoints[level.name].push_back(checkpoint_name)
+
+
+#func _unhandled_input(event: InputEvent) -> void:
+#	if event.is_action_pressed("restart"):
+## warning-ignore:return_value_discarded
+#		get_tree().reload_current_scene()
+#	elif event.is_action_pressed("DEBUG_die"):
+#		var last_checkpoint_name: String = visited_checkpoints[level.name].back()
+#		var last_checkpoint: Area2D = level.get_node("Checkpoints/" + last_checkpoint_name)
+#		$player.state_machine.transition_to("Die", {last_checkpoint = last_checkpoint})
+#	elif event.is_action_pressed("toggle_full_screen"):
+#		OS.window_fullscreen = not OS.window_fullscreen
