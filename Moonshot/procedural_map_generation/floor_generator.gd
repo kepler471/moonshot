5ea04cb0d4ecd89996c25048b6c1ba7ffccc6d25
whitew1994WW <will.white@dativa.com>
2020-11-15 17:43:30 +0000

    Commented out code not working

--- Moonshot/procedural_map_generation/floor_generator.gd
+++ Moonshot/procedural_map_generation/floor_generator.gd
@@ -9,6 +9,7 @@ const room_Count_Depth_Multiplier : float = 3.0
 var max_rooms = 100
 const room_Count_Variation = 0;
 var starting_room = 'room_base.tscn'
+var floor_level
 
 # 'map' will store the final dictionary of rooms, indexed by a vector
 var map = {}
@@ -23,12 +24,12 @@ var directions = {"LEFT": Vector2.LEFT, "UP": Vector2.DOWN, "RIGHT": Vector2.RIG
 var rng = RandomNumberGenerator.new()
 
 
-func _init(depth : int):
+func _init(level : int):
 	# Randomize seed
 	rng.randomize()
-	
+	floor_level = level
 	# Decide on a number of rooms for the floor, can be at max the original value
-	max_rooms = min(max_rooms, room_Count_Depth_Multiplier * depth + min_rooms + rng.randi_range(0,room_Count_Variation)) as int
+	max_rooms = min(max_rooms, room_Count_Depth_Multiplier * floor_level + min_rooms + rng.randi_range(0,room_Count_Variation)) as int
 	
 	# Add initial room & surround with rooms so that the start is always the same
 	var pos = Vector2.ZERO
@@ -58,7 +59,7 @@ func load_template_rooms():
 		if file == "":
 			break
 		elif file.begins_with("room"):
-			room_list.append(Room.new("", [], file))
+			room_list.append(Room.new("", [], 0, file))
 	return room_list
 
 
@@ -78,7 +79,7 @@ func grow():
 # Given the 2-D grid of room locations, assign a room template and room type
 func fill_with_rooms():
 	# Ensure that the starting room is always the same
-	add_Room(Room.new("Path", [], starting_room), Vector2(0, 0))
+	add_Room(Room.new("Path", [], floor_level, starting_room), Vector2(0, 0))
 	
 	# For all other rooms, identify the adjacent rooms i.e. connection requirements
 	# Then find a tempalte room with those requirementsand add to the map dictionary
@@ -130,7 +131,7 @@ func get_Room_With_Requirement (requires:Array, end:bool = false):
 		var rand_room = template_rooms[randi()%template_rooms.size()]
 		if arrays_match(rand_room.connections, requires):
 			# Copy the room_template
-			new_room = Room.new("", [], rand_room.get_template_name())
+			new_room = Room.new("", [], floor_level, rand_room.get_template_name())
 			# If a room with only one connection then select either BOSS, 
 			# REWARD or SHOP
 			if len(requires) == 1:
