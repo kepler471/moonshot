8874e1877e02a219c770d55ceea10a0d522aac14
Jamesbarford <jamesbarfordevans@gmail.com>
2020-11-15 13:02:12 +0000

    more cleverer bearboi

--- Moonshot/baddies/BearBoi.gd
+++ Moonshot/baddies/BearBoi.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 var Baddie = load("res://baddies/Baddie.gd").new()
-const GRAVITY := 8
+const GRAVITY := 10
 const SPEED := 230
 const HP_MAX := 1.0
 const FLOOR := Vector2(0, -1)
@@ -24,14 +24,21 @@ func _process(delta) -> void:
 	if Baddie == null:
 		call_deferred("free")
 		return
+
+	var collided_with_player: bool = Baddie.check_player_colision(true)
+	var falling_off_ledge: bool = $FrontRayCast.is_colliding() == false || $RearRayCast.is_colliding() == false
+	var collided_with_wall: bool = is_on_wall() && !collided_with_player
+	
+	if falling_off_ledge || collided_with_wall:
+		change_direction()
 		
 	Baddie.move(delta)
-	var collided_with_player = Baddie.check_player_colision(true)
-
-	if is_on_wall() && !collided_with_player:
-		Baddie.sprite.flip_h = !Baddie.sprite.flip_h
-		Baddie.direction = Baddie.change_direction()
 
 func on_hit(instance_id, damage) -> void:
 	if instance_id == self.get_instance_id():
 		Baddie.on_hit(damage)
+
+func change_direction() -> void:
+	$AnimatedSprite.flip_h = !$AnimatedSprite.flip_h
+	Baddie.direction = Baddie.change_direction()
+
