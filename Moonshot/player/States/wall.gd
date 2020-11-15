9d9fc4bad9a59131853a396a62e39bd1c9c4861d
Stelios Georgiou <steliosdg@outlook.com>
2020-11-15 16:30:56 +0000

    enable wall snap on collision, wall jump more vert height

--- Moonshot/player/States/wall.gd
+++ Moonshot/player/States/wall.gd
@@ -7,7 +7,7 @@ export var slide_acceleration := 1600.0
 export var max_slide_speed := 200.0
 export (float, 0.0, 1.0) var friction_factor := 0.15
 
-export var jump_strength := Vector2(500.0, 400.0)
+export var jump_strength := Vector2(500.0, 800.0)
 var _wall_normal := -1
 var _velocity := Vector2.ZERO
 
@@ -23,13 +23,13 @@ func physics_process(delta: float) -> void:
 	else:
 		_velocity.y += slide_acceleration * delta
 	_velocity.y = clamp(_velocity.y, -max_slide_speed, max_slide_speed)
-	_velocity = owner.move_and_slide(_velocity, owner.FLOOR_NORMAL)
+	_velocity = owner.move_and_slide_with_snap(_velocity, Vector2(-_wall_normal, 0), owner.FLOOR_NORMAL)
 
 	if owner.is_on_floor():
 		_state_machine.transition_to("Move/Idle")
 
 	var is_moving_away_from_wall := sign(_parent.get_move_direction().x) == sign(_wall_normal)
-	if is_moving_away_from_wall or not owner.ledge_wall_detector.is_against_wall():
+	if is_moving_away_from_wall:
 		_state_machine.transition_to("Move/Air", {"velocity":_velocity})
 
 	if owner.ledge_wall_detector.is_against_ledge():
