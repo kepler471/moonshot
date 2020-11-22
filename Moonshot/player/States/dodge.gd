09b333bec67d9931c77160e0bf7252bafec36694
Stelios Georgiou <steliosdg@outlook.com>
2020-11-22 14:01:06 +0000

    change player collision for dodge, add dodge cooldown timer

--- Moonshot/player/States/dodge.gd
+++ Moonshot/player/States/dodge.gd
@@ -1,27 +1,43 @@
 tool
 extends State
 
-onready var controls_freeze: Timer = $ControlsFreeze
+onready var dodge_period: Timer = $DodgePeriod
 
+var layer_default
+var mask_default
+const layer_dodge = 4 # Baddie layer
+const mask_dodge = 9 # Walls & PassThrough
 
-export var dodge_speed := 1250.0
+export var dodge_speed := 2500.0
 var dodge_dir
 
 
 func _get_configuration_warning() -> String:
-	return "" if $ControlsFreeze else "%s requires a Timer child named JumpDelay" % name
+	return "" if $DodgePeriod else "%s requires a Timer child named DodgePeriod" % name
 
 
 func physics_process(delta) -> void:
-	if owner.is_on_floor() and not controls_freeze.is_stopped():
+	if owner.is_on_floor() and not dodge_period.is_stopped():
 		_parent.velocity.x = dodge_speed * dodge_dir
 		_parent.physics_process(delta)
 	
-	if controls_freeze.is_stopped():
+	if dodge_period.is_stopped():
 		_parent.max_speed = _parent.max_speed_default
+		owner.set_collision_layer(layer_default)
+		owner.set_collision_mask(mask_default)
 		_state_machine.transition_to("Move/Run")
 
 func enter(_msg: Dictionary = {}) -> void:
-	controls_freeze.start()
+	dodge_period.start()
 	_parent.max_speed.x = dodge_speed
 	dodge_dir = owner.facing
+	
+	owner.set_invulnerable(dodge_period.get_wait_time(), "dodge")
+	
+	layer_default = owner.get_collision_layer()
+	mask_default = owner.get_collision_mask()
+	
+	# Changing the collisions layer and mask of Player during dodge
+	owner.set_collision_layer(4)
+	owner.set_collision_mask(9)
+	
--- Moonshot/player/player.gd
+++ Moonshot/player/player.gd
@@ -137,9 +137,8 @@ func _on_Player_health_depleted() -> void:
 	state_machine.transition_to("Die", {last_checkpoint = last_checkpoint})
 
 
-func set_invulnerable(time : float) -> void:
+func set_invulnerable(time : float, animation_name = "stagger") -> void:
 	invulnerable = true
-	animation_name = "stagger"
 	$AnimatedSprite.play(animation_name)
 	var timer = Utils.Player.get_tree().create_timer(time)
 	yield(timer, "timeout")
