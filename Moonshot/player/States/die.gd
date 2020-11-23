fbd906eb667f514c75d3983723ff8956017ee1a6
Stelios Georgiou <steliosdg@outlook.com>
2020-11-23 23:44:45 +0000

    cleared unused code

--- Moonshot/player/States/die.gd
+++ Moonshot/player/States/die.gd
@@ -4,14 +4,8 @@ extends State
 var last_checkpoint: Area2D = null
 
 
-func _on_Player_animation_finished(_anim_name: String) -> void:
-	_state_machine.transition_to('Spawn', {last_checkpoint = last_checkpoint})
-
-
-func enter(msg: Dictionary = {}) -> void:
-	assert("last_checkpoint" in msg)
-	last_checkpoint = msg.last_checkpoint
-
+func enter(_msg: Dictionary = {}) -> void:
+	pass
 
 func exit() -> void:
 	pass
