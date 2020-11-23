c5d9d1e168a151dce7744a875d3e667f1bca24c5
Stelios Georgiou <steliosdg@outlook.com>
2020-11-23 17:31:08 +0000

    stop rotating ghost, move to acceleration mechanics

--- Moonshot/baddies/characters/GhostyBoi.gd
+++ Moonshot/baddies/characters/GhostyBoi.gd
@@ -2,13 +2,16 @@ extends KinematicBody2D
 class_name GhostyBoi
 
 var attributes: Attributes = preload("res://baddies/Attributes.gd").new()
+var beam = load("res://baddies/BaddieLaser/Blob.tscn")
 var phi : float		# angle between self and target
 var theta : float	# global rotation
-var turn_max := 0.01
-
-const Animations := {
-	"RUSH": "rush"
-}
+var turn_max := PI
+var max_speed := 2.0
+var acceleration := 6.0
+var coll
+var damage := 0.02
+var cooldown := false
+var cooldown_timer :=  0.2
 
 
 func _get_configuration_warning() -> String:
@@ -20,9 +23,10 @@ func _init() -> void:
 
 	attributes.set_properties({
 		"body": self,
+		"direction": attributes.Direction.RIGHT,
 		"inital_hp": 5,
 		"gravity": 0,
-		"speed": 1,
+		"speed": max_speed,
 		"sprite": $AnimatedSprite,
 		"velocity": Vector2(),
 		"damage_to_player": 0.2,
@@ -35,28 +39,29 @@ func _ready():
 	$AnimatedSprite.play()
 	set_z_index(3)
 
+
 func _physics_process(delta) -> void:
-
 	theta = get_global_rotation()
-	phi = get_global_position().angle_to_point(Utils.Player.get_global_position()) - get_global_rotation()
-#	phi = min(abs(phi), turn_max) * sign(phi)
-	attributes.velocity = attributes.speed * Vector2.LEFT.rotated(get_global_rotation() + phi)
-	print(phi)
-	set_global_rotation(get_global_rotation() + phi)
-	attributes.body.move_and_collide(attributes.velocity)
+	phi = get_global_position().angle_to_point(Utils.Player.get_node("TurnAxis").get_global_position() - $RayCast2D.get_cast_to()) - theta
 
-func change_direction() -> void:
-	if !attributes._has_died():
-		attributes.set("direction", attributes._change_direction())
-		attributes.flip_sprite_horizontal()
+	attributes.velocity += Vector2.LEFT.rotated(theta + phi).normalized() * acceleration * delta
+	attributes.velocity = attributes.velocity.clamped(max_speed)
+	coll = attributes.body.move_and_collide(attributes.velocity)
+	
+	if $RayCast2D.is_colliding() and not cooldown:
+		drop_blob()
 
-		$FrontRayCast.async_change_direction()
 
-#
 #func on_hit(instance_id, damage) -> void:
 #	if instance_id == self.get_instance_id() && !attributes._has_died():
 #		change_direction()
 #		attributes._on_hit(damage, global_position)
 
-func on_end() -> void:
-	call_deferred("free")
+func drop_blob() -> void:
+	var shot = beam.instance()
+	shot.set_damage(damage)
+	get_tree().get_root().add_child(shot)
+	shot.set_global_position(get_global_position())
+	cooldown = true
+	yield(get_tree().create_timer(cooldown_timer), "timeout")
+	cooldown = false
