ebb5dd57136715283838287c4eb39f9874334304
whitew1994WW <will.white@dativa.com>
2020-11-26 12:07:31 +0000

    Major spawning overhaul

---
+++ Moonshot/procedural_map_generation/item_drop_chances.gd
@@ -0,0 +1,53 @@
+extends Node
+
+
+var item_weights: Dictionary = {
+'FireratePickup' : 30,
+'HealthPickup': 20,
+'FirerateDropUnlock': 3,
+'MachineGun': 10,
+'Shotgun':10,
+'TwinShot': 10}
+
+var item_paths: Dictionary = {
+'FireratePickup' : "res://items_objects/item_pickups/FireratePickup.tscn",
+'HealthPickup': "res://items_objects/item_pickups/HealthPickup.tscn",
+'FirerateDropUnlock': "res://items_objects/item_pickups/FirerateDropUnlock.tscn",
+'MachineGun': "res://items_objects/weapon_pickups/MachineGun.tscn",
+'Shotgun':"res://items_objects/weapon_pickups/Shotgun.tscn",
+'TwinShot': "res://items_objects/weapon_pickups/TwinShot.tscn"}
+
+
+# Fetches a random item based on the relative weights above and the items to select from
+func get_random_item(items):
+	var items_probabilities = {}
+	var item_probability_ranges = {}
+	var total_weight = 0
+	# Get the total weights
+	for item in items:
+		total_weight += item_weights[item]
+	
+	# Calculate the cumulative probabilities
+	var total = 0
+	for item in items:
+		total += item_weights[item]
+		items_probabilities[item] = float(total) / float(total_weight)
+	
+	# Calculate the probability ranges for each item 
+	item_probability_ranges[items[0]] = {'min': 0, 'max': items_probabilities[items[0]]}
+	for i in range(1, len(items)):
+		item_probability_ranges[items[i]] = {'min': items_probabilities[items[i-1]], 'max': items_probabilities[items[i]]}
+		
+	# Generate a random number between 0 and 1 and use it to sample the 
+	# discrete distribution
+	randomize()
+	var rand_float = rand_range(0, 1)
+	for item in items:
+		# If it is in the correct range then return an instance of the item
+		if rand_float > item_probability_ranges[item]['min'] && rand_float < item_probability_ranges[item]['max']:
+			var item_instance = load(item_paths[item]).instance()
+			return item_instance
+	
+		
+		
+	
--- Moonshot/room_templates/BaseRoom.gd
+++ Moonshot/room_templates/BaseRoom.gd
@@ -4,13 +4,205 @@ signal indicate_room_change(exit_key, entrance)
 
 const MAIN_GAME : bool = false
 
+
+var baddie_builder = load("res://procedural_map_generation/BaddieBuilder.gd")
+var DifficultyController = load("res://procedural_map_generation/DifficultyController.gd")
+var item_drop_chances = load("res://procedural_map_generation/item_drop_chances.gd").new()
+
+var selected_level_setup
+export var testing_room_difficulty = 1
+var scaling_factor = 0.75
+# Distance between enemy spawns on a line
+var min_baddie_spawn_distance = 32
+
+var item_spawn_nodes = []
+var baddie_spawn_lines = []
+var baddie_spawn_points = []
+var combined_baddie_spawns = []
+
 func _ready():
 	if get_tree().get_current_scene().MAIN_GAME == false:
 		self.add_child(Utils.Player)
 		Utils.Player.position = self.get_node("testing_player_spawn").position
 		self.setup_player_camera()
+		spawn_things(testing_room_difficulty)
+		
+	
+func spawn_things(room_difficulty):
+	randomize()
+	var setup_room_bool = select_level_setup()
+	var difficulty_controller : DifficultyController = DifficultyController.new(room_difficulty)
+	if setup_room_bool:
+		get_spawn_nodes()
+		spawn_baddies(difficulty_controller, room_difficulty)
+		spawn_items()
+	
+# Function to select a random level setup and remove the others from the room
+func select_level_setup():
+	# Retrieve the different level setups
+	var level_setups_node = find_node('level_setups')
+	if level_setups_node == null:
+		return false
+	var level_setups = level_setups_node.get_children()
+	selected_level_setup = level_setups[randi()%level_setups.size()]
+	# Delete all the level setups not nee
+	for level_setup in level_setups:
+		if not level_setup.name == selected_level_setup.name:
+			level_setup.queue_free()
+	return true
 
+# Fetches all the spawn nodes and stores them in the class for easier access
+func get_spawn_nodes():
+	var spawn_nodes = selected_level_setup.get_children()
+	for node in spawn_nodes:
+		if node.name.begins_with('baddie_spawn_line'):
+			baddie_spawn_lines.append(node)
+			combined_baddie_spawns.append(node)
+		if node.name.begins_with('baddie_spawn_point'):
+			baddie_spawn_points.append(node)
+			combined_baddie_spawns.append(node)
+		elif node.name.begins_with('item_spawn_point'):
+			item_spawn_nodes.append(node)
 
+			
+			
+# Spawns a number of enemies based on the level and size of the room
+func spawn_baddies(difficulty_controller: DifficultyController, room_difficulty):
+	# Minimum no of enemies must be less than 3
+	var min_no_baddies = min(len(baddie_spawn_lines) + len(baddie_spawn_points), 3)
+	var max_no_baddies = len(baddie_spawn_points)
+	
+	var distance
+	for line in baddie_spawn_lines:
+		var line_points = line.points
+		distance = line_points[0].distance_to(line_points[1])
+		max_no_baddies += distance / min_baddie_spawn_distance
+	max_no_baddies = round(max_no_baddies)
+	
+		
+	var no_baddies = difficulty_controller.get_no_baddies_in_room(room_difficulty, max_no_baddies, min_no_baddies)
+	
+	# Retrieve list of enemies and attributes of enemies (spawn on ground/air/wall + size + difficulty)
+	# Keeps track of the location of baddies on the spawn line so that they don't overlap and trigger spawn issues
+	var baddie_location_per_spawn_line = {}
+	# Decide on the baddie types for each spawn location
+	var baddie_type_per_spawn = set_spawn_baddie_type()
+	
+	# Spawn each baddie
+	for i in range(no_baddies):
+		# Select a random spawn point/line
+		var spawn_place = combined_baddie_spawns[randi()%combined_baddie_spawns.size()]
+		var baddie_name = baddie_type_per_spawn[spawn_place.name]
+
+		# This is here for demonstrative purposes and does nothing 
+		var baddie_instance = difficulty_controller.get_baddie_instance(baddie_name, room_difficulty)
+
+		# Create an empty dictionary to keep trackof where baddies have been placed
+		if not baddie_location_per_spawn_line.has(spawn_place.name):
+			baddie_location_per_spawn_line[spawn_place.name] = {}
+
+		# If it is a spawn point then remove the node from the
+		# combined_spawn areas
+		# If it is a line then duplicate instance and spawn randomly on the line
+		if spawn_place.get_class() == 'Position2D':
+			spawn_place.add_child(baddie_instance)
+			combined_baddie_spawns.erase(spawn_place)
+		else:
+			var line_start = spawn_place.points[0]
+			var line_end = spawn_place.points[1]
+			var point_difference_vector = line_end - line_start
+			var rand_step_options = shuffle_list(range(0, round(point_difference_vector.length() / min_baddie_spawn_distance)))
+			var chosen_step
+			for step in rand_step_options:
+				if not baddie_location_per_spawn_line[spawn_place.name].has(step):
+					baddie_location_per_spawn_line[spawn_place.name][step] = true
+					chosen_step = step
+					break
+					
+			if chosen_step == null:
+				continue
+					
+			var random_point = line_start + chosen_step*(point_difference_vector / min_baddie_spawn_distance)
+
+			print("The line_start is: "+ str(line_start))
+			print("The line_end is: " + str(line_end))
+			print("The point_difference_vector is: " + str(point_difference_vector))
+			print("The random_point is: " + str(random_point))
+
+			spawn_place.add_child(baddie_instance)
+			baddie_instance.position = random_point
+
+func set_spawn_baddie_type() -> String:
+	var spawn_baddie_type = {}
+	var selected_baddie
+	var baddie_instance
+	var new_baddie_instance 
+	var baddie_list = get_baddie_list()
+	# Decide on the baddies that are allocated for each spawn poinmt
+	for spawn_place in combined_baddie_spawns:
+		if len(spawn_place.get_children()) == 0:
+			selected_baddie = "res://baddies/characters/" + baddie_list[randi()%baddie_list.size()]
+		else:
+			baddie_instance = spawn_place.get_children()[0]
+			# Remove the existing instances
+			spawn_place.remove_child(baddie_instance)
+			baddie_instance.queue_free()
+			
+			# Retrieve the location of the enemy scene
+			selected_baddie = baddie_instance.script.resource_path
+			selected_baddie = selected_baddie.replace('.gd', '.tscn')
+		
+		# Load the baddie scene
+		spawn_baddie_type[spawn_place.name] = selected_baddie
+	return spawn_baddie_type
+	
+# Spawnsmitems at the item locations
+func spawn_items():
+	# Retrieve the list of default items
+	var default_possible_items = item_drop_chances.item_weights.keys()
+	var possible_items
+	for node in item_spawn_nodes:
+		var item_children = node.get_children()
+		var item_names = []
+		for item in item_children:
+			item_names.append(item.name)
+			item.queue_free()
+		
+		# If there are item children on the node then use those items as the default
+		if len(item_children) > 0:
+			possible_items = item_names
+		else:
+			possible_items = default_possible_items
+		
+		var item_instance = item_drop_chances.get_random_item(possible_items)
+		item_instance.global_position = node.global_position
+		node.get_parent().add_child(item_instance)
+
+	
+func get_baddie_list():
+	var dir = Directory.new()
+	dir.open("res://baddies/characters/")
+	dir.list_dir_begin()
+	var baddie_filenames = []
+	while true:
+		var file = dir.get_next()
+		if file == "":
+			break
+		elif file.ends_with(".tscn"):
+			baddie_filenames.append(file)	
+	return baddie_filenames
+	
+func shuffle_list(list):
+	var shuffled_list = []
+	var index_list = range(list.size())
+	for i in range(list.size()):
+		randomize()
+		var x = randi()%index_list.size()
+		shuffled_list.append(list[x])
+		index_list.remove(x)
+		list.remove(x)
+	return shuffled_list
+	
 func setup_player_camera():
 	# Set the player to have the camera
 	var player_camera = Utils.Player.get_node('Camera2D')
