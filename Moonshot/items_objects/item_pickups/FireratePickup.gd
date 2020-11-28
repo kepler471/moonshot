bfe68b3d501a99060b58baf553efa723c8fb898b
Jamesbarford <jamesbarfordevans@gmail.com>
2020-11-28 23:20:47 +0000

    fixes: firerate performance, empty array for baddies and animation being connected

--- Moonshot/items_objects/item_pickups/FireratePickup.gd
+++ Moonshot/items_objects/item_pickups/FireratePickup.gd
@@ -1,33 +1,34 @@
 extends RigidBody2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 const PICK_UP_TRAVEL_SPEED = 0.01
 const FIRE_RATE_INCREASE = 1
-# Called when the node enters the scene tree for the first time.
+const PLAYER_SIGNAL = "emit_player_global_position_drop"
+const MOVE_TO_PLAUER = "move_to_player"
+
 func _ready():
-	CombatSignalController.connect("emit_player_global_position_drop", self, "move_to_player")
+	_connect()
 
-	
 func move_to_player(player_global_position):
+	if !is_inside_tree() && _is_connected():
+		CombatSignalController.disconnect(PLAYER_SIGNAL, self, MOVE_TO_PLAUER);
+	else:
+		if !_is_connected():
+			_connect()
+		float_to_player(player_global_position)
+
+func float_to_player(player_global_position) -> void:
 	var shot_direction: Vector2 = (player_global_position - get_global_position()).normalized()
+	position = self.global_position + (shot_direction * 0.01)
+	apply_central_impulse(shot_direction * PICK_UP_TRAVEL_SPEED)
+	rotation = PI + position.angle_to_point(player_global_position)
 
-	self.position = self.global_position + (shot_direction * 0.01)
-	self.apply_central_impulse(shot_direction * PICK_UP_TRAVEL_SPEED)
-	self.rotation = PI + self.position.angle_to_point(player_global_position)
+func _connect() -> void:
+	CombatSignalController.connect(PLAYER_SIGNAL, self, MOVE_TO_PLAUER)
 
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
+func _is_connected() -> bool:
+	return CombatSignalController.is_connected(PLAYER_SIGNAL, self, MOVE_TO_PLAUER)
 
 func _on_FirerateDrop_body_entered(body):
 	if body.is_in_group('Player'):
 		Utils.player_stats.pickup_firerate(FIRE_RATE_INCREASE)
 		self.queue_free()
-
