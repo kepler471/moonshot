2381af68924bc9c5b1a866b32e6ae1166dc83c92
Stelios Georgiou <steliosdg@outlook.com>
2020-11-28 17:49:38 +0000

    lower health, fix collision layer to overlap terrain, fade on death momentarily

--- Moonshot/baddies/characters/MotherHen.gd
+++ Moonshot/baddies/characters/MotherHen.gd
@@ -15,9 +15,9 @@ func _init() -> void:
 		"body": self,
 		"animation": Animations.FLOAT,
 		"speed": 0,
-		"inital_hp": 40.0,
+		"inital_hp": 10.0,
 		"gravity": 1,
-		"damage_to_player": 0.2,
+		"damage_to_player": 0.05,
 		"floor_vector": Vector2(0, -1),
 		"should_damage_on_collision": true
 	})
@@ -25,11 +25,25 @@ func _init() -> void:
 func _ready():
 	attributes.set_sprite($AnimatedSprite)
 	spawner.spawn_randomly()
+	
+func _physics_process(delta):
+	move_and_slide(Vector2.ZERO)
+	var _q = attributes._check_player_colision()
 
 func on_hit(instance_id, damage) -> void:
 	if instance_id == self.get_instance_id() && !attributes._has_died():
 		spawner.spawn_randomly()
-		attributes._on_hit(damage, global_position)
+		attributes.hp -= damage
+		attributes._flash()
+		if attributes.hp <= 0:
+			death_transition()
+
+func death_transition() -> void:
+	attributes.set_properties({"body": self, "should_damage_on_collision": false})
+	on_end()
 
 func on_end() -> void:
-	attributes.set_properties({"should_damage_on_collision": false})
+	attributes.fade.set_fade_speed(0.05)
+	attributes.fade.set_fade_factor(0.3)
+	attributes.dead = true
+	attributes.fade.fade_out()
