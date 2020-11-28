c0200c8a80d064182c6752e65074242a66fcc94f
wattsit <billjwatts@gmail.com>
2020-11-28 23:03:01 +0000

    Next level lift logic

---
+++ Moonshot/room_templates/LiftButton.gd
@@ -0,0 +1,18 @@
+extends Sprite
+
+var red = load("res://assets/environment/LiftButtonRed.png")
+var green = load("res://assets/environment/LiftButtonGreen.png")
+var on = load("res://assets/environment/LiftButtonON.png")
+
+func _ready():
+	set_texture(red)
+
+func change_texture(state):
+	match state:
+		"red":
+			set_texture(red)
+		"green":
+			set_texture(green)
+		"on":
+			set_texture(on)
+			print("I AM RUNNING")
