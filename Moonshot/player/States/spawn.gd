0d858644e8d02bf8a80f5b328efce8eee325bdc4
Stelios Georgiou <steliosdg@outlook.com>
2020-11-29 04:53:32 +0000

    reparent to Move

--- Moonshot/player/States/spawn.gd
+++ Moonshot/player/States/spawn.gd
@@ -6,7 +6,7 @@ onready var safe_period: Timer = $SafePeriod
 
 
 func _get_configuration_warning() -> String:
-	return "" if $DodgePeriod else "%s requires a Timer child named SpawnPeriod" % name
+	return "" if $SafePeriod else "%s requires a Timer child named SafePeriod" % name
 
 
 func unhandled_input(event: InputEvent) -> void:
@@ -15,15 +15,8 @@ func unhandled_input(event: InputEvent) -> void:
 
 func enter(msg: Dictionary = {}) -> void:
 	if "room" in msg:
-		owner.set_invulnerable(safe_period.get_wait_time(), "idle")
-
-#		layer_default = owner.get_collision_layer()
-#		mask_default = owner.get_collision_mask()
-#
-#		# Changing the collisions layer and mask of Player during dodge
-#		owner.set_collision_layer(4)
-#		owner.set_collision_mask(9)
+		owner.set_invulnerable(2, "spawn")
 	_state_machine.transition_to("Move/Idle")
 
 func exit() -> void:
-	pass
+	_parent.exit()
