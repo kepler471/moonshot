0e6d22f5123a74974075f0567f54d46dfa57d475
wattsit <67764860+wattsit@users.noreply.github.com>
2020-12-01 20:38:53 +0000

    menu update (#303)

--- Moonshot/Menu/Menu.gd
+++ Moonshot/Menu/Menu.gd
@@ -1,8 +1,7 @@
 extends Node2D
 
 func _ready():
-	$Player.get_node("Camera2D").set_zoom(Vector2(0.75,0.75))
-	$Player.get_node("Camera2D").position += Vector2(0,-150)
+	$Player.get_node("Camera2D").position += Vector2(0,-120)
 
 func activate_lift():
 	$ExitLift.activate_lift()
