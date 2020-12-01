82a69f9f1414c7a024a6092756ac084d9de6a308
wattsit <billjwatts@gmail.com>
2020-12-01 12:06:43 +0000

    Menu Buttons Enhancement

--- Moonshot/Menu/Menu.gd
+++ Moonshot/Menu/Menu.gd
@@ -1,8 +1,5 @@
 extends Node2D
 
-var inside_exit = false
-
-
 func _ready():
 	add_child(Utils.Player)
 	Utils.Player.position = Vector2(-137,153)
@@ -10,15 +7,3 @@ func _ready():
 	Utils.Player.get_node("Camera2D").position += Vector2(0,-150)
 	Utils.Player.state_machine.transition_to("Move/Spawn", {"room": true})
 	Utils.Player.safety = true
-
-func _input(event):
-	if event.is_action_pressed("enter") and inside_exit:
-		get_tree().quit()
-		
-
-func _on_ExitButton_body_entered(body):
-	inside_exit = true
-
-
-func _on_ExitButton_body_exited(body):
-	inside_exit = false
