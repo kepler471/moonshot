303ff9f9e83efaacb82e199bb3a7af2d0bb233e0
whitew1994WW <will.white@dativa.com>
2020-12-03 09:33:31 +0000

    Forgot to change the other weapons as well

--- Moonshot/items_objects/weapon_pickups/TwinShot.gd
+++ Moonshot/items_objects/weapon_pickups/TwinShot.gd
@@ -5,7 +5,7 @@ extends Node2D
 func _on_Area2D_body_entered(body):
 	if body.is_in_group('Player'):
 		Utils.player_arsenal.set_weapon("twin_shot")
+		Utils.player_arsenal.get_weapon().pickup_ammo()
 		Utils.player_arsenal.update_hud()
-		Utils.player_arsenal.reset_arsenal()
 		body.set_arm_sprite("twin_shot")
 		queue_free()
